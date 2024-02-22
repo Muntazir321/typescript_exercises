23af3372226c7e68d2e50ec47222650af4986e5f
Muntazir <143006603+Muntazir321@users.noreply.github.com>
2024-02-22 11:21:06 -0800

    Delete 2. Personal Message directory

--- 2. Personal Message/main.js
+++
@@ -1,6 +0,0 @@
-// 1st Way
-var Name = "Eric";
-console.log("Hello " + Name + "," + "would you like to learn some Python today?");
-// 2nd Way
-var Name2 = "Eric";
-console.log("Hello ".concat(Name2, ", would you like to learn some python today?"));
--- 2. Personal Message/main.ts
+++
@@ -1,7 +0,0 @@
-// 1st Way
-let Name = "Eric";
-console.log("Hello " + Name + "," + "would you like to learn some Python today?");
-
-// 2nd Way
-let Name2 = "Eric";
-console.log(`Hello ${Name2}, would you like to learn some python today?`)
